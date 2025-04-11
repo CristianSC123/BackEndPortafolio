728520a7ec7abccf2173d1092ec2531913b6b77e
Cristian <cristiaansantacruz@gmail.com>
2025-04-11 00:46:25 -0400

    Quitando dotenv

--- apps/Analisis/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import requests
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('distiluse-base-multilingual-cased')
-
-def generar_embedding(texto):
-    return model.encode(texto).tolist()
