5f06251588fd6581bdca94d11160f6922832e38e
Cristian <cristiaansantacruz@gmail.com>
2025-04-11 00:54:19 -0400

    Quitando dotenv

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://portafoliocriss.up.railway.app",
-    "https://backendportafolio-production-cb7a.up.railway.app/"
+    "https://backendportafolio-production-cb7a.up.railway.app"
 ]
 
 
@@ -44,7 +44,7 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://portafoliocriss.up.railway.app/', 'https://backendportafolio-production-cb7a.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'https://portafoliocriss.up.railway.app/', 'https://backendportafolio-production-cb7a.up.railway.app']
 
 
 INSTALLED_APPS = [
